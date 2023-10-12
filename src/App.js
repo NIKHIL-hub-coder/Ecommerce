9df459d6831ce43b1fe0240ba0addbee804f130f
NIKHIL-hub-coder <78261687+NIKHIL-hub-coder@users.noreply.github.com>
2023-10-12 09:40:56 +0530

    filter and sort changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import ProductList from './features/product-list/components/ProductList';
+import ProductList from './features/product/components/ProductList';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import Signup from './features/auth/components/Signup';
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -58,7 +58,7 @@ function Checkout() {
                   Personal Information
                 </h2>
                 <p className="mt-1 text-sm leading-6 text-gray-600">
-                  Use a permanent address where you can receive mail.
+                  Use a permanent address where you can receive package.
                 </p>
 
                 <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -130,9 +130,9 @@ function Checkout() {
                         autoComplete="country-name"
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                       >
+                        <option>India</option>
                         <option>United States</option>
                         <option>Canada</option>
-                        <option>Mexico</option>
                       </select>
                     </div>
                   </div>
@@ -178,7 +178,7 @@ function Checkout() {
                       htmlFor="region"
                       className="block text-sm font-medium leading-6 text-gray-900"
                     >
-                      State / Province
+                      State
                     </label>
                     <div className="mt-2">
                       <input
@@ -228,7 +228,7 @@ function Checkout() {
 
               <div className="border-b border-gray-900/10 pb-12">
                 <h2 className="text-base font-semibold leading-7 text-gray-900">
-                Addresses
+                Address:
                 </h2>
                 <p className="mt-1 text-sm leading-6 text-gray-600">
                   Choose from Existing addresses
